78af2c62e23d2ae3cdd3372b72726ccc55069e91
Surendra <surendra.waso@gmail.com>
2020-06-19 16:45:35 +0530

    agentcreation page changes

--- NodeCode/api/agentdetails/createagent/controller.js
+++ NodeCode/api/agentdetails/createagent/controller.js
@@ -64,6 +64,7 @@
                 res.status(200).json({status: true, message: "Success", data: agentPasswd });
             }
         }).catch((err)=>{
+			console.log(err);
             res.status(400).json({status: false, message: err, data: null });
         });
 
